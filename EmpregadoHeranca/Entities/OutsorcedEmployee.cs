73e21291f25dfce1d0266ac0f341b6c7159f3a4c
Antonio Carlos <antoniolp_92@hotmail.com>
2019-10-09 20:46:54 -0300

    Exercicio de teste utilizando herança de classes

---
+++ EmpregadoHeranca/Entities/OutsorcedEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpregadoHeranca.Entities
+{
+    class OutsorcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsorcedEmployee()
+        {
+        }
+
+        public OutsorcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
---
+++ EmpregadoHeranca/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using EmpregadoHeranca.Entities;
+using System.Collections.Generic;
+
+namespace EmpregadoHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsorced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                if (ch == 'y')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsorcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                { 
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee employee in list)
+            {
+                Console.WriteLine(employee.Name + " - $" + employee.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
